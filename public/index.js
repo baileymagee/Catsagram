c7ce0794df8d3d1994774b3b987aef24d9775cb4
BaileyMagee <wolf217c@gmail.com>
2022-11-22 15:34:27 -0600

    kitten title

--- public/index.js
+++ public/index.js
@@ -1,1 +1,16 @@
-// Your code here
+window.onload = () => {
+      // create a new div element
+      const newElement = document.createElement("h1");
+
+      // set the h1's id
+      newElement.setAttribute("id", "kitten_title");
+
+      // and give it some content
+      const newContent = document.createTextNode("Kitten Pic");
+
+      // add the text node to the newly created div
+      newElement.appendChild(newContent);
+
+      // add the newly created element and its content into the DOM
+      document.body.appendChild(newElement);
+}
